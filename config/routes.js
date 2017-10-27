1203fec5c4a29c078dc464fe1da6f0eb7baf9011
annineedscoffee <char.anne.spencer@gmail.com>
2017-10-27 12:02:26 -0700

    draft

--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,12 @@ const index = require("../controllers/index.js");
 module.exports = function(app){
 
   app.get('/', index.main);
+  app.get('/pokemon', index.pkmn);
+  app.get('/show/pokemon/:id', index.shw);
+  app.get('/edit/pokemon/:id', index.edit);
+  app.get('/add/pokemon', index.added);
+  app.post('/add/pokemon', index.nmon);
+  app.get('/trainers', index.train);
+  app.get('/show/trainers/:id', index.shwtr);
+  app.get('/gym', index.gym);
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-var knex = require('knex');
+var knex = require('../db/knex.js');
 
 module.exports = {
 
@@ -6,5 +6,85 @@ module.exports = {
     res.render('index', { title: 'Express' });
   },
 
+  pkmn: function(req, res){
+    knex('pokemon').then((result)=>{
+      res.render('pkmn', {pokemon: result})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
 
+  shw: function(req, res){
+    knex('pokemon')
+    .where('id', req.params.id)
+    .then((result)=>{
+      console.log("my result", result)
+    res.render('showpkmn',{pokemon: result[0]})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+
+  edit: function(req, res, next){
+    knex('pokemon')
+    .where('id', req.params.id)
+      .then((result)=>{
+        res.render('editpkmn', {pokemon: result[0]})
+      })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+
+  added: function(req, res, next){
+    res.render('addpkmn', {title: 'Express'});
+  },
+
+  nmon: function(req, res){
+    knex('pokemon')
+    .insert({
+      name: req.body.name,
+      cp: req.body.cp,
+      in_gym: req.body.in_gym,
+      trainer_id: req.body.trainer_id
+    }, "*")
+    .then((result)=>{
+      console.log(result);
+      res.redirect("/pokemon")
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+
+  train: function(req, res){
+    knex('trainers').then((result)=>{
+      res.render('trainers', {trainers: result})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+
+  shwtr: function(req, res){
+    knex('trainers')
+  .where('id', req.params.id)
+    .then((result)=>{
+      res.render('showtrainers', {trainers: result[0]})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+
+  gym: function(req, res){
+    knex('pokemon').then((result)=>{
+      res.render('gym', {pokemon: result})
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
 };
